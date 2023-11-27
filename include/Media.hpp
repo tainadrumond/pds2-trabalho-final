2dcea06ba71b11edfe18c961f1c3fa5deacd28f2
OhLaurent <gabri.laurentino4000@gmail.com>
2023-11-27 09:43:49 -0300

    Media.hpp

---
+++ include/Media.hpp
@@ -0,0 +1,31 @@
+#ifndef MEDIA_HPP
+#define MEDIA_HPP
+
+#include <string>
+
+class Media
+{
+private:
+    static int _idCounter;
+
+    int _id;
+    std::string _title;
+    int _copies;
+    float _price;
+
+public:
+    Media(int id, std::string title, int copies, float price) : _id(id), _title(title), _copies(copies), _price(price)
+    {
+        _idCounter++;
+    };
+
+    int getId();
+    std::string getTitle();
+    int getCopies();
+    float getPrice();
+
+    void rent();
+    void virtual returnMedia();
+};
+
+#endif

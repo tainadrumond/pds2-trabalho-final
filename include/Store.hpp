41a086c3fbd552de567cc0d0a5e6e87f8bc1a7ad
OhLaurent <gabri.laurentino4000@gmail.com>
2023-12-02 21:30:12 -0300

    Updates store.hpp

--- include/Store.hpp
+++ include/Store.hpp
@@ -4,28 +4,30 @@
 #include "Client.hpp"
 #include "Media.hpp"
 #include "Rent.hpp"
+#include "Extinguisher.hpp"
 #include <map>
 #include <vector>
 
-class Store {
-    private:
-        std::map<int, Client*> _clientsByCpf;
-        std::map<int, Media*> _mediasById;
-        std::map<int, Extinguisher> _ExtinguishersById;
-        std::vector<Rent*> _rents;
-        float income;
+class Store
+{
+private:
+    std::map<int, Client *> _clientsByCpf;
+    std::map<int, Media *> _mediasById;
+    std::map<int, Extinguisher> _ExtinguishersById;
+    std::vector<Rent *> _rents;
+    float income;
 
-    public:
-        void setClient(Client* client);
-        void setMedia(Media* media);
-        void setRent(Rent* rent);
+public:
+    void setClient(Client *client);
+    void setMedia(Media *media);
+    void setRent(Rent *rent);
 
-        Client* getClient(int cpf);
-        Media* getMedia(int id);
-        Rent* getRent(int cpf);
-        float getIncome();
+    Client *getClient(int cpf);
+    Media *getMedia(int id);
+    Rent *getRent(int cpf);
+    float getIncome();
 
-        void calculateIncome();
+    void calculateIncome();
 };
 
 #endif

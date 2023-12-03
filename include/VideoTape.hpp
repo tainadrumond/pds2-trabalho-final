2c91978d1df3ed10a50fef5e551440cef13ec557
John Carvs <joaovitor772@hotmail.com>
2023-12-03 18:39:30 -0300

    Comentários a respeito de john

--- include/VideoTape.hpp
+++ include/VideoTape.hpp
@@ -13,11 +13,39 @@ private:
     bool const _fixedPrice = true;
 
 public:
+  /**
+ * @brief Construtor do VideoTape
+ *
+ * Constrói um objeto VideoTape de acordo com determinados parâmetros.
+ *
+ * @param title Título do filme representado.
+ * @param copies Quantidade de cópias.
+ * @param ID Número de identificação usado.
+ */
     VideoTape(std::string title, int copies, int id) : Media(id, title, copies, VIDEO_TAPE_PRICE, _fixedPrice){};
-
+  /**
+ * @brief Método que informa o preço da fita.
+ *
+ * Retorna o preço do objeto que o chamou.
+ *
+ * @return Atributo do preço.
+ */
     float getPrice();
+     /**
+ * @brief Método que informa o preço da fita rebobinada.
+ *
+ * Retorna o preço do objeto que o chamou.
+ *
+ * @return Atributo do preço.
+ */
     float getPriceForRewoundDevolution();
-
+ /**
+ * @brief Método que informa o tipo de mídia.
+ *
+ * Retorna o tipo da mídia do objeto que o chamou.
+ *
+ * @return Tipo da mídia.
+ */
     std::string getMediaType();
 };
 

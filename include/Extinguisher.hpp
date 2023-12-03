214a34de13a8ca1de6cb860694ec26d751e07f02
John Carvs <joaovitor772@hotmail.com>
2023-12-03 19:57:14 -0300

    Mais comentários 😳

--- include/Extinguisher.hpp
+++ include/Extinguisher.hpp
@@ -14,9 +14,25 @@ private:
   bool _type; // 0 for water; 1 for CO2
 
 public:
+/**
+* @brief Método que constrói um extintor.
+*/
   Extinguisher(int daysToExpDate, bool type) : _daysToExpDate(daysToExpDate), _type(type){};
+/**
+* @brief Método que recarrega o extintor.
+*/
   void refuel();
+/**
+* @brief Método informa os dias para trocar o extintor.
+*
+* @return Int de dias para expirar.
+*/
   int getDaysToExpDate();
+/**
+* @brief Método informa os dias para trocar o extintor.
+*
+* @return Booleano do tipo.
+*/
   bool getType();
 };
 
